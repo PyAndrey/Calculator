fef62054bfb5165fc30a406b4b909f5d84345843
PyAndrey <yuzvuk.andrey02@mail.ru>
2021-12-08 19:24:52 +0300

    refac

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 
-
-
 window = tk.Tk()
+
 
 def main():
     greeting = tk.Label(text="0", bg="black", fg="white")
@@ -10,206 +9,186 @@ def main():
 
     # 1 ряд
     button = tk.Button(
-    text="CE",
-    width=25,
-    height=10,
-    activebackground="#606060",
-    background="#404040",
-    activeforeground="white",
-    foreground="white",
-    highlightcolor="yellow",
-    bd=0
+        text="CE",
+        width=25,
+        height=10,
+        activebackground="#606060",
+        background="#404040",
+        activeforeground="white",
+        foreground="white",
+        highlightcolor="yellow",
+        bd=0,
     )
     button.grid(column=0, row=1)
     button = tk.Button(
-    text="C",
-    width=25,
-    height=10,
-    activebackground="#606060",
-    background="#404040",
-    activeforeground="white",
-    foreground="white",
-    border=0
+        text="C",
+        width=25,
+        height=10,
+        activebackground="#606060",
+        background="#404040",
+        activeforeground="white",
+        foreground="white",
+        border=0,
     )
     button.grid(column=1, row=1)
     button = tk.Button(
-    text="<",
-    width=25,
-    height=10,
-    activebackground="#606060",
-    background="#404040",
-    activeforeground="white",
-    foreground="white",
-    border=0
+        text="<",
+        width=25,
+        height=10,
+        activebackground="#606060",
+        background="#404040",
+        activeforeground="white",
+        foreground="white",
+        border=0,
     )
     button.grid(column=2, row=1)
     button = tk.Button(
-    text="/",
-    width=25,
-    height=10,
-    activebackground="#606060",
-    background="#404040",
-    activeforeground="white",
-    foreground="white",
-    border=0
+        text="/",
+        width=25,
+        height=10,
+        activebackground="#606060",
+        background="#404040",
+        activeforeground="white",
+        foreground="white",
+        border=0,
     )
     button.grid(column=3, row=1)
 
     # 2 ряд
     button = tk.Button(
-    text="7",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-
+        text="7",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=0, row=2)
     button = tk.Button(
-    text="8",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="8",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=1, row=2)
     button = tk.Button(
-    text="9",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="9",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=2, row=2)
     button = tk.Button(
-    text="\u00D7",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
+        text="\u00D7",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=3, row=2)
 
     # 3 ряд
     button = tk.Button(
-    text="4",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="4",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=0, row=3)
     button = tk.Button(
-    text="5",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="5",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=1, row=3)
     button = tk.Button(
-    text="6",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="6",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=2, row=3)
     button = tk.Button(
-    text="-",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="-",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=3, row=3)
 
     # 4 ряд
     button = tk.Button(
-    text="1",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="1",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=0, row=4)
     button = tk.Button(
-    text="2",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="2",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=1, row=4)
     button = tk.Button(
-    text="3",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="3",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=2, row=4)
     button = tk.Button(
-    text="+",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="+",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=3, row=4)
 
     # 5 ряд
     button = tk.Button(
-    text="+/-",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="+/-",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=0, row=5)
     button = tk.Button(
-    text="0",
-
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="0",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=1, row=5)
     button = tk.Button(
-    text=".",
-
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text=".",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=2, row=5)
     button = tk.Button(
-    text="=",
-    width=6,
-    height=2,
-    bg="#404040",
-    fg="white",
-    
+        text="=",
+        width=6,
+        height=2,
+        bg="#404040",
+        fg="white",
     )
     button.grid(column=3, row=5)
-
-
-
 
 
 if __name__ == "__main__":
