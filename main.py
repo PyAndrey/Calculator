9e76abf0c5b16633a36682110dd226832afbc653
PyAndrey <yuzvuk.andrey02@mail.ru>
2022-02-12 21:38:24 +0300

    Complete

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """
-Beta 1.1
+Beta 3.0
 Calculator by Andrei Yuzvuk
-16.01.2022
+12.02.2022
 Bugs:
-    1. Может быть две точки в числе
-    2. 45 + 6 + 6 = 516 при двух плюсах добавляется ко 2 числу
+    1. 45 + 6 + 6 = 516 - добавить функционал
 """
 from tkinter import *
 from tkinter import ttk
@@ -23,11 +22,12 @@ class Calculator:
     window.resizable(False, False)
 
     def __init__(self) -> None:
-        self.a = ""  # Первое число
-        self.b = ""  # Второе число
+        self.num_first = 0  # Первое число
+        self.num_second = 0  # Второе число
         self.sign = ""  # Знак операции
+        self.result = 0 # Результат
         self.finish = False # Была нажата кнопка = ?
-        self.digit = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".")
+        self.digit = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
         self.action = ("/", "\u00D7", "-", "+")
         self.label_text = StringVar(self.window, "0")
 
@@ -77,124 +77,99 @@ class Calculator:
 
     def clear_last(self, value: str) -> None:
         """Кнопка CE очистки последнего"""
-        self.b = ""  # Второе число
+        self.num_second = 0  # Второе число
         self.finish = False # Была нажата кнопка = ?
         self.label_text.set("0")
 
     def clear_all(self, value: str) -> None:
         """Кнопка C очистки всего"""
-        self.a = ""  # Первое число
-        self.b = ""  # Второе число
+        self.num_first = 0  # Первое число
+        self.num_second = 0  # Второе число
         self.sign = ""  # Знак операции
+        self.result = 0 # Результат
         self.finish = False # Была нажата кнопка = ?
         self.label_text.set("0")
     
 
     def backspace(self, value: str) -> None:
         """Кнопка удаления последней цифры"""
-        if self.a != '' and self.sign == '' and self.b == '':
-            self.a = self.a[0:-1]
-            self.label_text.set(self.a)
-            if not self.a:
-                self.label_text.set("0")
-        elif self.a != '' and self.sign != '' and self.b != '':
-            self.b = self.b[0:-1]
-            self.label_text.set(self.b)
-            if not self.b:
-                self.label_text.set("0")
-
-    def isfloat(self, value: str) -> bool:
-        """Проверяет строку на число float"""
-        try:
-            if '.' in value:
-                float(value)
-                return True
-        except ValueError:
-            return False
+        if len(self.label_text.get()) == 1:
+            self.label_text.set("0")
+        else:
+            self.label_text.set(self.label_text.get()[:-1])
+    
+    def dot_button_press(self, value):
+        """Добавляет в label точку"""
+        # Если в строке нету точки, то она добавляется
+        if self.label_text.get().count(".") == 0:
+            self.label_text.set(self.label_text.get() + ".")
 
     def number_button_press(self, value: str) -> None:
         """По нажатию на кнопку с цифрой"""
         # Если нажата кнопка 0-9 или .
         if value in self.digit:
-            # Если второе число пустое и знак пустой
-            if self.b == '' and self.sign == '':
-                self.a += value
-                print(f'{self.a=}')
-                self.label_text.set(self.a)
-            # Пофиксить баг
-            elif self.a != '' and self.b != '' and self.sign != '':
-                self.b = value
-                self.finish = False
-                self.label_text.set(self.b)
-            # Если первое число не пустое и второе число не пустое и нажата равно
-            elif self.a != '' and self.b != '' and self.finish:
-                self.b = value
-                self.finish = False
-                self.label_text.set(self.b)
+            if self.label_text.get() == "0" or self.label_text.get() in self.action:
+                self.label_text.set("" + value)
             else:
-                self.b += value
-                print(f'{self.b=}')
-                self.label_text.set(self.b)
+                self.label_text.set(self.label_text.get() + value)
+
+    def plus_minus_button_press(self, value):
+        # Если нажата +/-
+        if self.label_text.get().count(".") != 0:
+            all_numbers  = float(self.label_text.get())
+        else:
+            all_numbers  = int(self.label_text.get())
+        all_numbers *= -1
+        self.label_text.set(all_numbers)
 
     def math_button_press(self, value: str) -> None:
         """По нажатию на математические знаки"""
         # Если нажата кнопка + - / *
         if value in self.action:
-            self.sign = value
+            # Проверка строки на наличие числа float
+            if self.label_text.get().count(".") != 0:
+                self.num_first  = float(self.label_text.get())
+            else:
+                self.num_first  = int(self.label_text.get())
+
+            if self.sign == "":
+                self.sign = value
+
             print(f'{self.sign=}')
             self.label_text.set(self.sign)
-        
-        # Если нажата +/-
-        if value == '+/-':
-            print(f'{value=}')
-            # Если первое число не пустое и второе пустое
-            if self.a != '' and self.b == '':
-                # Первое число становится отрицательным
-                self.a = f"-{self.a}"
-                self.label_text.set(self.a)
-            # Если первое число не пустое и второе не пустое
-            elif self.a != '' and self.b != '':
-                #  Второе число становится отрицательным
-                self.b = f"-{self.b}"
-                self.label_text.set(self.b)
-
+            
     def equal_button_press(self, value: str) -> None:
         """По нажатию на равно"""
-        # Нажата =
-        if value == '=':
-            # Если есть первое число и знак, то прибавляется первое число к первому чилу
-            if self.b == '': self.b = self.a
+        # Если есть первое число и знак, то прибавляется первое число к первому чилу
+        if self.num_second == 0: self.num_second = self.num_first
 
-            # Преобразование во float если возможно
-            if self.isfloat(self.a) or self.isfloat(self.b):
-                self.a = float(self.a)
-                self.b = float(self.b)
-            else:
-                self.a = int(self.a)
-                self.b = int(self.b)
+        if self.label_text.get().count(".") != 0:
+            self.num_second  = float(self.label_text.get())
+        else:
+            self.num_second  = int(self.label_text.get())
 
-            match self.sign:
-                case '+':
-                    self.a = self.a + self.b
-                case '-':
-                    self.a = self.a - self.b
-                case '/':
-                    try:
-                        self.a = round(self.a / self.b, 4)
-                    except ZeroDivisionError:
-                        self.label_text.set("Ошибка")
-                        messagebox.showwarning(message="Ты долбаёб? В школе не учили? На ноль делить нельзя!!!")
-                        self.a = ""  # Первое число обнулилось
-                        self.b = ""  # Второе число обнулилось
-                        self.sign = "" # Знак обнулился
-                        return
-                case '\u00D7': # Умножить
-                    self.a = self.a * self.b
-            self.a = str(self.a)
-            self.b = str(self.b)
-            self.finish = True
-            print(f'{self.a=}')
-            self.label_text.set(self.a)    
+        match self.sign:
+            case '+':
+                self.result = self.num_first + self.num_second
+            case '-':
+                self.result = self.num_first - self.num_second
+            case '/':
+                try:
+                    self.result = round(self.num_first / self.num_second, 4)
+                except ZeroDivisionError:
+                    self.label_text.set("0")
+                    messagebox.showwarning(message="Ты долбаёб? В школе не учили? На ноль делить нельзя!!!")
+                    self.num_first = 0  # Первое число обнулилось
+                    self.num_second = 0  # Второе число обнулилось
+                    self.sign = "" # Знак обнулился
+                    return
+            case '\u00D7': # Умножить
+                self.result = self.num_first * self.num_second
+
+        self.finish = True
+        print(f'{self.result=}')
+        self.label_text.set(self.result) 
+
 
     def main(self) -> None:
         """Тело приложения"""
@@ -221,9 +196,9 @@ class Calculator:
         self.create_btn(4, 2, "3", self.number_button_press)
         self.create_btn(4, 3, "+", self.math_button_press)
         # 5 ряд
-        self.create_btn(5, 0, "+/-", self.math_button_press)
+        self.create_btn(5, 0, "+/-", self.plus_minus_button_press)
         self.create_btn(5, 1, "0", self.number_button_press)
-        self.create_btn(5, 2, ".", self.number_button_press)
+        self.create_btn(5, 2, ".", self.dot_button_press)
         self.create_btn(5, 3, "=", self.equal_button_press)
 
     def start(self) -> None:
